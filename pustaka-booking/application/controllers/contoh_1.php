23b141dbd722318be20dccdffa469bad2e2408d3
rizkikaykay <rizkinur0304@gmail.com>
2022-04-19 13:53:44 +0700

    first commit

---
+++ pustaka-booking/application/controllers/contoh_1.php
@@ -0,0 +1,13 @@
+<?php
+
+class contoh_1 extends CI_Controller {
+public function index()
+	{
+		echo "Biodata Saya<br>";
+		echo "Nama Lengkap : Rizki Nur Apriansyah<br>";
+		echo "Nim : 17190010<br>";
+		echo "Fakultas : Teknik dan Informasi<br>";
+		echo "Jurusan : Teknologi Informasi<br>";
+		echo "Jenis Kelamin : Laki-Laki";
+	}
+}
